7e96730c0d8760dc7a16bf0486e4c12c5d8663aa
nkumar041 <nikumar041@gmail.com>
2023-10-19 16:58:04 -0400

    fixed

--- aws.py
+++ aws.py
@@ -1,5 +1,5 @@
 
-import boto3
+# import boto3
 
-s3 = boto3.resource('s3')
-s3.meta.client.upload_file('model.joblib', 'sagemaker-studio-dpky3i1jbvs', 'model.joblib')
+# s3 = boto3.resource('s3')
+# s3.meta.client.upload_file('model.joblib', 'sagemaker-studio-dpky3i1jbvs', 'model.joblib')
