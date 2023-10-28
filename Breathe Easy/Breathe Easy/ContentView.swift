085e235e345ead38ee2b39a78bd8ad8f6a78359f
akuppa9 <amogh.kuppa9@gmail.com>
2023-10-28 16:44:19 -0400

    added icon

--- Breathe Easy/Breathe Easy/ContentView.swift
+++ Breathe Easy/Breathe Easy/ContentView.swift
@@ -4,21 +4,6 @@
 //
 //  Created by Amogh Kuppa on 10/28/23.
 //
-/*func apiCall() -> String{
-    if let url = URL(string: "http://127.0.0.1:5000/22.0/2.54/0.77/4.84/1040.0/98.0/0.51/0.0/2.0/283.72/20.39/21.99/2.27/11.64/16.40/0.39/83.0") {
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                return
-            }
-            
-            if let data = data {
-                if let stringData = String(data: data, encoding: .utf8) {
-                    return "hi"
-                }
-            }
-        }
-    }
-}*/
 
 import Foundation
 // Usage of the function
@@ -40,9 +25,9 @@ struct ContentView: View {
         guard let url=URL(string:endpoint) else{
             throw GHError.invalidURL
         }
-                
+        
         let (data,response) = try await URLSession.shared.data(from:url)
-                
+        
         guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {throw GHError.invalidResponse}
         do{
             let decoder = JSONDecoder()
@@ -55,27 +40,54 @@ struct ContentView: View {
     @State private var user: Pf?
     var body: some View {
         ZStack{
-            if user?.value ?? 0 > 0 && user?.value ?? 0 <= 0.30{
+            if user?.value ?? 0 >= 0 && user?.value ?? 0 <= 0.30{
                 ContainerRelativeShape().fill(.red.gradient)
-            }
-            else if user?.value ?? 0 > 0.30 && user?.value ?? 0 <= 0.60{
-                ContainerRelativeShape().fill(.yellow.gradient)
-            }
-            else if user?.value ?? 0 > 0.60{
-                ContainerRelativeShape().fill(.green.gradient)
-            }
-            
-            VStack {
-                Text("Your Peak Flow Percentage")
-                    .font(.system(size: 24, weight: .bold))
-                Image(systemName: "lungs")
-                    .imageScale(.large)
-                    .foregroundColor(.black)
-                Text("\(user?.value ?? 40) %")
-                    .font(.title2)
-                    }
-            
+                    .ignoresSafeArea()
+                VStack {
+                    Text("Your Peak Flow Percentage")
+                        .font(.system(size: 30, weight: .bold))
+                        .position(x:197, y:200)
+                    Image(systemName: "lungs")
+                        .imageScale(.large)
+                        .foregroundColor(.black)
+                    Text("\(user?.value ?? 0) %")
+                        .font(.title2)
+                    Text("Your Peak Flow has fallen below 50% of your predicted. You may be at severe risk of an asthma attack").font(.title3).multilineTextAlignment(.center)
+                        .position(x:197, y:200)
                 }
+                
+            }
+            else if user?.value ?? 0 > 0.50 && user?.value ?? 0 < 0.70{
+                ContainerRelativeShape().fill(.yellow.gradient)
+                    .ignoresSafeArea()
+                VStack {
+                    Text("Your Peak Flow Percentage")
+                        .font(.system(size: 24, weight: .bold))
+                    Image(systemName: "lungs")
+                        .imageScale(.large)
+                        .foregroundColor(.black)
+                    Text("\(user?.value ?? 0) %")
+                        .font(.title2)
+                    Text("Your Peak Flow is between 50% and 80% of your personal best. You may be at moderate risk of an asthma attack")
+            }
+            }
+            else if user?.value ?? 0 > 0.70 && user?.value ?? 0 <= 1.0{
+                ContainerRelativeShape().fill(.green.gradient)
+                    .ignoresSafeArea()
+                VStack {
+                    Text("Your Peak Flow Percentage")
+                        .font(.system(size: 24, weight: .bold))
+                    Image(systemName: "lungs")
+                        .imageScale(.large)
+                        .foregroundColor(.black)
+                    Text("\(user?.value ?? 0) %")
+                        .font(.title2)
+                    Text("Your Peak Flow is above 80% of your personal best. You can breathe easy")
+            }
+            }
+            
+        }
+        
         .task{
             do{
                 user = try await getPf()
@@ -89,13 +101,8 @@ struct ContentView: View {
                 print("Error")
             }
         }
-            }
-        }
-
-        #Preview {
-            ContentView()
-        }
-
+    }
+}
 
 #Preview {
     ContentView()
