4dc3f1db91bdb97e831ca1ddd5241d915dc912a1
Athin Shetty <athinshetty@outlook.com>
2023-10-28 09:16:08 -0400

    finished flask apis

--- flaskapis.py
+++ flaskapis.py
@@ -6,9 +6,10 @@ import joblib
 app = Flask(__name__)
 
 
-@app.route('/<int:hr>/<float:temp>/<float:temp_min>/<float:temp_max>/<float:humidity>/<float:wind_speed>/<float:wind_deg>/<float:aqi>')
-def hello(temp,temp_min,temp_max,humidity,wind_speed,wind_deg,aqi,hr):
+@app.route('/<float:hour>/<float:temperature>/<float:temperature_min>/<float:temperature_max>/<float:pressure>/<float:humidity>/<float:wind_speed>/<float:wind_deg>/<float:aqi>/<float:co>/<float:no2>/<float:o3>/<float:so2>/<float:pm2_5>/<float:pm10>/<float:nh3>/<float:hr>/')
+def hello(hour, temperature, temperature_min, temperature_max, pressure, humidity, wind_speed, wind_deg, aqi, co, no2, o3, so2, pm2_5, pm10, nh3, hr):
     model = joblib.load('asthma_model.joblib')
-
-    prediction = model.predict([[temp,temp_min,temp_max,humidity,wind_speed,wind_deg,aqi,hr,0,0,0,0,0,0,0,0,0]])
+    prediction = model.predict([[hour, temperature, temperature_min, temperature_max, pressure, humidity, wind_speed, wind_deg, aqi, co, no2, o3, so2, pm2_5, pm10, nh3, hr]])
     return json.dumps({"value" : prediction[0]})
+
+# http://127.0.0.1:5000/22.0/2.54/0.77/4.84/1040.0/98.0/0.51/0.0/2.0/283.72/20.39/21.99/2.27/11.64/16.40/0.39/83.0 test url
--- getrequests.swift
+++
@@ -1,8 +0,0 @@
-let url = URL(string: "http://www.stackoverflow.com")!
-
-let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-    guard let data = data else { return }
-    print(String(data: data, encoding: .utf8)!)
-}
-
-task.resume()
