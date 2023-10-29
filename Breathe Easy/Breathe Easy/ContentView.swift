3f6a234386ca17190fc9beb27297f3005b45b526
nkumar041 <nikumar041@gmail.com>
2023-10-28 20:16:33 -0400

    TabBar

--- Breathe Easy/Breathe Easy/ContentView.swift
+++ Breathe Easy/Breathe Easy/ContentView.swift
@@ -31,18 +31,16 @@ enum GHError: Error{
     case invalidData
 }
 
-import SwiftUI
-
-struct ContentView: View {
+struct ViewA: View{
     func getPf(url: String) async throws -> Pf{
         let endpoint = url
         
         guard let url=URL(string:endpoint) else{
             throw GHError.invalidURL
         }
-                
+        
         let (data,response) = try await URLSession.shared.data(from:url)
-                
+        
         guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {throw GHError.invalidResponse}
         do{
             let decoder = JSONDecoder()
@@ -57,40 +55,64 @@ struct ContentView: View {
     
     var body: some View {
         ZStack{
-            if user?.value ?? 0 >= 0 && user?.value ?? 0 <= 0.30{
-                ContainerRelativeShape().fill(.red.gradient).ignoresSafeArea()
+            if user?.value ?? 0 >= 0 && user?.value ?? 0 < 0.50{
+                Color(UIColor.lightGray).ignoresSafeArea()
+                Text("Peak Flow")
+                    .font(.system(size: 35, weight: .bold)).multilineTextAlignment(.center).position(x: 197, y: 200)
+                Image("lung50t1").resizable().frame(width: 300, height: 300)
+                Text(String(format: "%.0f%%",(user?.value ?? 0)*100))
+                    .font(.system(size: 35)).position(x:197,y:550)
             }
-            else if user?.value ?? 0 > 0.30 && user?.value ?? 0 <= 0.60{
-                ContainerRelativeShape().fill(.yellow.gradient).ignoresSafeArea()
+            else if user?.value ?? 0 >= 0.50 && user?.value ?? 0 < 0.60{
+                Color(UIColor.lightGray).ignoresSafeArea()
+                Text("Peak Flow")
+                    .font(.system(size: 35, weight: .bold)).multilineTextAlignment(.center).position(x: 197, y: 200)
+                Image("60tfixed").resizable().frame(width: 300, height: 300)
+                Text(String(format: "%.0f%%",(user?.value ?? 0)*100))
+                    .font(.system(size: 35)).position(x:197,y:550)
             }
-            else if user?.value ?? 0 > 0.60{
-                ContainerRelativeShape().fill(.green.gradient).ignoresSafeArea()
+            else if user?.value ?? 0 >= 0.60 && user?.value ?? 0 < 0.70{
+                Color(UIColor.lightGray).ignoresSafeArea()
+                Text("Peak Flow")
+                    .font(.system(size: 35, weight: .bold)).multilineTextAlignment(.center).position(x: 197, y: 200)
+                Image("70tfixed").resizable().frame(width: 300, height: 300)
+                Text(String(format: "%.0f%%",(user?.value ?? 0)*100))
+                    .font(.system(size: 35)).position(x:197,y:550)
+            }
+            else if user?.value ?? 0 >= 0.70 && user?.value ?? 0 < 0.80{
+                Color(UIColor.lightGray).ignoresSafeArea()
+                Text("Peak Flow")
+                    .font(.system(size: 35, weight: .bold)).multilineTextAlignment(.center).position(x: 197, y: 200)
+                Image("80t").resizable().frame(width: 300, height: 300)
+                Text(String(format: "%.0f%%",(user?.value ?? 0)*100))
+                    .font(.system(size: 35)).position(x:197,y:550)
+            }else if user?.value ?? 0 >= 0.80 {
+                Color(UIColor.lightGray).ignoresSafeArea()
+                Text("Peak Flow")
+                    .font(.system(size: 35, weight: .bold)).multilineTextAlignment(.center).position(x: 197, y: 200)
+                Image("80plust").resizable().frame(width: 300, height: 300)
+                Text(String(format: "%.0f%%",(user?.value ?? 0)*100))
+                    .font(.system(size: 35)).position(x:197,y:550)
             }
             
-            Text("Peak Flow")
-                .font(.system(size: 35, weight: .bold)).multilineTextAlignment(.center).position(x: 197, y: 200)
-            Image(systemName: "lungs")
-                .imageScale(.large).position(x: 197,y: 300)
-                .foregroundColor(.black)
-            Text(String(format: "%.0f%%",(user?.value ?? 0)*100))
-                .font(.system(size: 35)).position(x:197,y:550)
             
             
             
-//            VStack {
-//                Text("Peak Flow")
-//                    .font(.system(size: 24, weight: .bold)).multilineTextAlignment(.center).position(x: 197, y: 200)
-//                Image(systemName: "lungs")
-//                    .imageScale(.large).position(x: 197,y: 220)
-//                    .foregroundColor(.black)
-//                Text(String(format: "%.0f%%",(user?.value ?? 0)*100))
-//                    .font(.title2).position(x:197)
-//                    }
             
-                }
+            //            VStack {
+            //                Text("Peak Flow")
+            //                    .font(.system(size: 24, weight: .bold)).multilineTextAlignment(.center).position(x: 197, y: 200)
+            //                Image(systemName: "lungs")
+            //                    .imageScale(.large).position(x: 197,y: 220)
+            //                    .foregroundColor(.black)
+            //                Text(String(format: "%.0f%%",(user?.value ?? 0)*100))
+            //                    .font(.title2).position(x:197)
+            //                    }
+            
+        }
         .task{
             do{
-                user = try await getPf(url: "http://127.0.0.1:5000/22.0/4.5/4.54/4.54/1040.0/98.0/0.51/0.0/2.0/283.72/20.39/21.99/2.27/11.64/16.40/0.39/83.0")
+                user = try await getPf(url: "http://127.0.0.1:5000/22.0/15.0/15.0/10.0/1040.0/98.0/2.51/0.0/2.0/283.72/20.39/21.99/2.27/11.64/16.40/0.39/83.0")
             } catch GHError.invalidURL{
                 print("invalid URL")
             }catch GHError.invalidResponse{
@@ -101,13 +123,49 @@ struct ContentView: View {
                 print("Error")
             }
         }
-            }
-        }
+        
+    }
+}
 
-        #Preview {
-            ContentView()
+struct ViewB: View{
+    var body: some View{
+        ZStack{
+            Color.red
         }
+    }
+}
 
+struct ViewC: View{
+    var body: some View{
+        ZStack{
+            Color.blue
+        }
+    }
+}
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View{
+        TabView{
+            ViewA()
+                .tabItem(){
+                    Image(systemName: "phone.fill")
+                    Text("Calls")
+                }
+            ViewB()
+                .tabItem(){
+                    Image(systemName: "person.2.fill")
+                    Text("Calls")
+                }
+            ViewC()
+                .tabItem(){
+                    Image(systemName: "person.fill")
+                    Text("Calls")
+                }
+        }
+    }
+}
 
 #Preview {
     ContentView()
